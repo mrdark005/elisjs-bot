8c30da56a3132f530695a12a49fc10796309cc98
MrDark <63722795+mrdark005@users.noreply.github.com>
2021-07-21 11:21:03 +0600

    Create nsfw.js

---
+++ commands/nsfw.js
@@ -0,0 +1,34 @@
+const superagent = require("superagent");
+
+module.exports = {
+  name: "nsfw",
+  description: "Ping!",
+  execute: async (message, client, args) => {
+    if (!args[0]) return message.channel.send("");
+    if (
+      args[0] !== "4k" &&
+      args[0] !== "anal" &&
+      args[0] !== "thigh" &&
+      args[0] !== "holo" &&
+      args[0] !== "ass" &&
+      args[0] !== "hentai"
+    ) return message.channel.send("please specify a category available categories; 4k, anal, thigh, holo, ass, hentai ")
+      return message.channel.send("There is no category named **" + args[0]) + "**";
+    if (message.channel.nsfw) {
+      superagent
+        .get("https://nekobot.xyz/api/image")
+        .query({ type: args[0] })
+        .end((err, response) => {
+          message.channel.send({
+            embeds: [
+              {
+                title: args[0],
+                image:response.body.message
+              }
+            ]
+          });
+        });
+    } else
+      message.channel.send("this channel is not available for this command");
+  }
+};
