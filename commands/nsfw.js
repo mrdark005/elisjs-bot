76bcf27038306c2020010a6470a951aa650fbc6e
MrDark <63722795+mrdark005@users.noreply.github.com>
2021-07-21 11:30:21 +0600

    Update nsfw.js

--- commands/nsfw.js
+++ commands/nsfw.js
@@ -4,7 +4,10 @@ module.exports = {
   name: "nsfw",
   description: "Ping!",
   execute: async (message, client, args) => {
-    if (!args[0]) return message.channel.send("There is no category named **" + args[0]) + "**";
+    if (!args[0])
+      return (
+        message.channel.send("There is no category named **" + args[0]) + "**"
+      );
     if (
       args[0] !== "4k" &&
       args[0] !== "anal" &&
@@ -12,7 +15,10 @@ module.exports = {
       args[0] !== "holo" &&
       args[0] !== "ass" &&
       args[0] !== "hentai"
-    ) return message.channel.send("please specify a category available categories; 4k, anal, thigh, holo, ass, hentai ")
+    )
+      return message.channel.send(
+        "please specify a category available categories; 4k, anal, thigh, holo, ass, hentai "
+      );
     if (message.channel.nsfw) {
       superagent
         .get("https://nekobot.xyz/api/image")
@@ -22,7 +28,9 @@ module.exports = {
             embeds: [
               {
                 title: args[0],
-                image:response.body.message
+                image: {
+                  url: response.body.message
+                }
               }
             ]
           });
