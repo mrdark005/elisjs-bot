1dbda82faede6c1efb6019a41926f8f6025b27ae
MrDark <63722795+mrdark005@users.noreply.github.com>
2021-07-21 11:20:27 +0600

    Create help.js

---
+++ commands/help.js
@@ -0,0 +1,18 @@
+module.exports = {
+  name: "help",
+  description: "Command List!",
+  execute: async (message, client, args) => {
+    var commandList = "";
+    client.commands.forEach(e => {
+      commandList += `!${e.name} > ${e.description} \n\n`;
+    });
+    message.channel.send({
+      embeds: [
+        {
+          title: "ElisJS - Command List",
+          description: commandList,
+        }
+      ]
+    });
+  }
+};
