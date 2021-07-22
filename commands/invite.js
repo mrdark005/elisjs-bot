e5f986c40d7d77af251883b9466d23db14564261
MrDark <63722795+mrdark005@users.noreply.github.com>
2021-07-22 19:32:59 +0600

    Update invite.js

--- commands/invite.js
+++ commands/invite.js
@@ -1,5 +1,5 @@
 module.exports = {
-  name: "help",
+  name: "invite",
   description: "add a bot.",
   execute: async (message, client, args) => {
     var message = "Merhaba, ben elisjs ile yapılmış ilk botum. \n\n [beni ekle](https://discord.com/oauth2/authorize?client_id=867386136145428531&scope=bot&permissions=0) \n [GITHUB](https://github.com/mrdark005/elisjs-bot) [GLITCH](https://glitch.com/edit/#!/elisjs)";
